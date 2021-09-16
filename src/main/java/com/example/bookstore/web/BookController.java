5e66c1a6f8c627ae214568453fa6bf7680c14590
jazmc <jasmin.lumme@gmail.com>
2021-09-16 09:11:56 +0300

    Added comments and removed unused imports

--- src/main/java/com/example/bookstore/web/BookController.java
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -1,8 +1,5 @@
 package com.example.bookstore.web;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,8 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.bookstore.domain.Book;
 import com.example.bookstore.domain.BookRepository;
@@ -21,27 +16,34 @@ public class BookController {
 	@Autowired
 	private BookRepository repository;
 
-	public BookController(BookRepository repository) {
-		this.repository = repository;
-	}
-
+	//perussivu /booklist
 	@RequestMapping("/booklist")
 	public String bookList(Model model) {
+		//lisätään kirjat modeliin
 		model.addAttribute("books", repository.findAll());
+		//booklist.html
 		return "booklist";
 	}
 
+	// lisäyslomake /add 
 	@PostMapping("/add")
 	public String postBook(String title, String author, String isbn, String year) {
+		//luodaan kirjaolio parametreinä saaduista string-muuttujista
 		Book newbook = new Book(isbn, title, author, year);
+		//tallennus
 		repository.save(newbook);
+		//uudelleenohjaus perussivulle
 		return "redirect:/booklist";
 	}
 
+	// poistolinkki /delete/id
 	@GetMapping("/delete/{no}")
 	public String deleteBook(@PathVariable String no) {
+		//string longiksi
 		long id = Long.parseLong(no);
+		//poisto
 	    repository.deleteById(id);
+	    //uudelleenohjaus perussivulle
 	    return "redirect:/booklist";
 	}
 }
