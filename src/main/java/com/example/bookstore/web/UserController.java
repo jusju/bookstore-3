fa54660f825bd389dae9994f00736be7a73c10e0
jazmc <jasmin.lumme@gmail.com>
2021-10-06 21:38:31 +0300

    Revert "Päivitystä"
    This reverts commit d597b0f1d60c75fb166b558865564129d1aa9bb2.

--- src/main/java/com/example/bookstore/web/UserController.java
+++ src/main/java/com/example/bookstore/web/UserController.java
@@ -20,10 +20,10 @@ public class UserController {
 	@Autowired
     private UserRepository repository; 
 	
-    @RequestMapping(value = "rekisteroidy")
+    @RequestMapping(value = "signup")
     public String addStudent(Model model){
     	model.addAttribute("signupform", new Signup());
-        return "rekisteroidy";
+        return "signup";
     }	
     
     /**
@@ -51,18 +51,18 @@ public class UserController {
 		    	}
 		    	else {
 	    			bindingResult.rejectValue("username", "err.username", "Username already exists");    	
-	    			return "rekisteroidy";		    		
+	    			return "signup";		    		
 		    	}
     		}
     		else {
     			bindingResult.rejectValue("passwordCheck", "err.passCheck", "Passwords does not match");    	
-    			return "rekisteroidy";
+    			return "signup";
     		}
     	}
     	else {
-    		return "rekisteroidy";
+    		return "signup";
     	}
-    	return "redirect:/kirjaudu";    	
+    	return "redirect:/login";    	
     }    
     
 }
