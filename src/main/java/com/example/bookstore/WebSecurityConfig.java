dcc6886c1ef420744bb33c3fc40a09ceeafa264b
jusju <jukka.juslin@haaga-helia.fi>
2021-10-07 11:17:18 +0300

    added true flag to websec config

--- src/main/java/com/example/bookstore/WebSecurityConfig.java
+++ src/main/java/com/example/bookstore/WebSecurityConfig.java
@@ -39,7 +39,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
           .and()
       .formLogin()
 			.loginPage("/login")
-			.defaultSuccessUrl("/booklist")
+			.defaultSuccessUrl("/booklist", true)
 			.permitAll()
 		.and()
 			.logout()
